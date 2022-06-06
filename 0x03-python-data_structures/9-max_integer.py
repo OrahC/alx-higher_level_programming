ee27f86d2ce39e4866db76cc2c39028758136b4d
OrahC <ifeanyirh@gmail.com>
2022-06-07 00:02:02 +0100

    done

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-    if len(sentence) == 0:
-        return len(sentence), None
-    return len(sentence), sentence[0]
+def max_integer(my_list=[]):
+    length = len(my_list)
+
+    if length == 0:
+        return (None)
+
+    max_int = my_list[0]
+
+    for i in range(1, length):
+        if my_list[i] > max_int:
+            max_int = my_list[i]
+
+    return (max_int)
